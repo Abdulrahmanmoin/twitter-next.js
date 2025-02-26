06368bd0eef882748fbe24d388f3c6d083fbbba0
Abdulrahmanmoin <iam.armoin@gmail.com>
2025-02-26 18:24:20 +0500

    Made changes in auth error page - make a state var to display err msg

--- src/app/(auth)/auth/error/page.tsx
+++ src/app/(auth)/auth/error/page.tsx
@@ -2,26 +2,27 @@
 
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
-import { Suspense, useEffect } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useToast } from "@/hooks/use-toast";
 
 function ErrorContent() {
   const searchParams = useSearchParams();
   const errorType = searchParams.get("error");
   const { toast } = useToast();
+  const [errorMessage, setErrorMessage] = useState("An error occurred while signing in.")
 
-  let message = "An error occurred while signing in.";
   useEffect(() => {
     if (errorType) {
-      // Customize the message based on error type
-      if (errorType === "This email is already registered using credentials. Please log in with your password.") {
-        message = "This email is already registered using credentials. Please log in with your email and password.";
-      } else if (errorType === "This email is already using Google. Please log in with Google.") { 
-        message = "This email is already using Google. Please log in with Google."
-      }
+      // // Customize the message based on error type
+      // if (errorType === "This email is already registered using credentials. Please log in with your password.") {
+      //   message = "This email is already registered using credentials. Please log in with your email and password.";
+      // } else if (errorType === "This email is already using Google. Please log in with Google.") {
+      //   message = "This email is already using Google. Please log in with Google."
+      // }
+      setErrorMessage(errorType)
       toast({
         title: "Sign In Error",
-        description: message,
+        description: errorMessage,
         variant: "destructive"
       });
     }
@@ -31,9 +32,7 @@ function ErrorContent() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
       <h1 className="text-3xl font-bold">Sign In Error</h1>
       <p className="mt-4">
-        {errorType
-          ? message
-          : "An unknown error occurred."}
+        {errorMessage}
       </p>
       <Link href="/signup" className="mt-6 text-blue-400 hover:underline">
         Go back to Sign Up
